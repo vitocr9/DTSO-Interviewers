c3288f43a8d6bf17e6364a84a2a9238e83a17416
Victorino_Cazares <Victorino_Cazares@EPMXGUAW0501>
2020-09-20 13:13:47 -0500

    Adding Skills and Competencies pages

---
+++ DTSOInterviewers/Controllers/CompetenciesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DTSOInterviewers.Models;
+
+namespace DTSOInterviewers.Controllers
+{
+    public class CompetenciesController : Controller
+    {
+        private DEV_InterviewersEntities db = new DEV_InterviewersEntities();
+
+        // GET: Competencies
+        public ActionResult Index()
+        {
+            return View(db.Competencies.ToList());
+        }
+
+        // GET: Competencies/Details/5
+        public ActionResult Details(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competency competency = db.Competencies.Find(id);
+            if (competency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competency);
+        }
+
+        // GET: Competencies/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Competencies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCompetency,Description,Stakeholder")] Competency competency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Competencies.Add(competency);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(competency);
+        }
+
+        // GET: Competencies/Edit/5
+        public ActionResult Edit(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competency competency = db.Competencies.Find(id);
+            if (competency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competency);
+        }
+
+        // POST: Competencies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCompetency,Description,Stakeholder")] Competency competency)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(competency).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(competency);
+        }
+
+        // GET: Competencies/Delete/5
+        public ActionResult Delete(byte? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Competency competency = db.Competencies.Find(id);
+            if (competency == null)
+            {
+                return HttpNotFound();
+            }
+            return View(competency);
+        }
+
+        // POST: Competencies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte id)
+        {
+            Competency competency = db.Competencies.Find(id);
+            db.Competencies.Remove(competency);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DTSOInterviewers/Controllers/SkillsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DTSOInterviewers.Models;
+
+namespace DTSOInterviewers.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private DEV_InterviewersEntities db = new DEV_InterviewersEntities();
+
+        // GET: Skills
+        public ActionResult Index()
+        {
+            return View(db.Skills.ToList());
+        }
+
+        // GET: Skills/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // GET: Skills/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdSkill,Description")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Skills.Add(skill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(skill);
+        }
+
+        // GET: Skills/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdSkill,Description")] Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(skill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(skill);
+        }
+
+        // GET: Skills/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            Skill skill = db.Skills.Find(id);
+            db.Skills.Remove(skill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
